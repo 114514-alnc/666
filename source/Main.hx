b1927178883d29097ba11db56397692b70002449
roblox-script <yangguoying221@gmail.com>
2026-03-28 06:57:34 +0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -131,7 +131,8 @@ class Main extends Sprite
 		if (game.zoom == -1.0)
 			game.zoom = 1.0;
 		#end
-	
+	ClientPrefs.loadPrefs();
+
 		#if LUA_ALLOWED Lua.set_callbacks_function(cpp.Callable.fromStaticFunction(psychlua.CallbackHandler.call)); #end
 		Controls.instance = new Controls();
 		ClientPrefs.loadDefaultKeys();
